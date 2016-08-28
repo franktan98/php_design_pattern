7bdfdf46bd333c31ad72579d298b3f82e0d2ee01
franktan98 <franktan98@yahoo.com>
2016-08-28 16:03:43 +0800

    edit some documentation on this class

--- creational/prototype/prototype.php
+++ creational/prototype/prototype.php
@@ -1,48 +1,41 @@
 <?php
-class Book_Record{
-	private $bookname ;
-	private $price ; 
-	
-	public function __construct(){
-	}
+/*
+ * This file contain BookRecord Class
+ */
+/**
+ * Description of prototype
+ * this class is a prototype Design Pattern class.
+ * by the research use clone is more faster then create a new object.
+ *
+ * @author franktan98
+ */
+class BookRecord {
+    private $bookname;
+    private $price;
 
-	public function __clone(){
-	}
+    public function __construct() {
+        echo  'object created';
+    }
 
-	public function set_bookname($source_bookname){
-		$this->bookname = $source_bookname ; 
-	}
+    public function __clone() {
+        echo 'Object cloned';
+    }
 
-	public function set_price($source_price){
-		$this->price = $source_price ; 
-	}
+    public function set_bookname($source_bookname) {
+        $this->bookname = $source_bookname;
+    }
 
-	public function get_book(){
-		$return_value = array();
+    public function set_price($source_price) {
+        $this->price = $source_price;
+    }
 
-		$return_value['bookname'] = $this->bookname;
-		$return_value['price'] = $this->price ; 
+    public function get_book() {
+        $return_value = array();
+        
+        $return_value['bookname'] = $this->bookname;
+        $return_value['price'] = $this->price;
 
-		return $return_value ;
-	}
+        return $return_value;
+    }
 }
-
-$temp = new Book_Record();
-$temp->set_bookname('SQL');
-$temp->set_price(9);
-echo var_dump($temp->get_book() );
-
-$temp1 = clone $temp;
-$temp1->set_bookname('PHP');
-$temp1->set_price(10);
-echo var_dump($temp1->get_book() );
-
-$temp3 = clone $temp;
-$temp3->set_bookname('Master PHP');
-$temp3->set_price(112);
-echo var_dump($temp3->get_book() );
-
-
-
-
 ?>
